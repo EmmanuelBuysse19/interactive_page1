da8ebfccebd4cce90ee394b3a89723134ec4a08b
Emmanuel Buysse <buysse19@gmail.com>
2022-02-10 17:52:39 -0600

    added new file

---
+++ index.js
@@ -0,0 +1,7 @@
+let canvas = document.getquerySelector("#mycanvas");
+let ctx = canvas.getContext("2d");
+const ballRadius = 8;
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+<button onclick="getquerySelector(#readmore).innerHTML = Date()">What is the time?</button>
